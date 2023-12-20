c4c2112b250a6eb304e7e8165b06a961f333961b
Serik Ayazbayev <jetheez@yandex.ru>
2023-12-20 22:06:44 +0600

    feat: update lead text

--- src/components/Lead/Lead.tsx
+++ src/components/Lead/Lead.tsx
@@ -4,9 +4,10 @@ export const Lead = ({}: LeadProps) => {
   return (
     <section className="pt-[100px] sm:pt-32">
       <div className="max-w-screen-lg w-full">
-        <p className="text-2xl font-normal sm:text-6xl">
-          Для получения возможности участия в закрытых распродажах, проводимых
-          акциях и получения персональных
+        <p className="text-2xl font-normal sm:text-5xl">
+          Для доступа к эксклюзивным распродажам, участия в специальных
+          мероприятиях и получения персонализированных предложений, просим Вас
+          заполнить следующие поля анкеты.
         </p>
       </div>
     </section>
