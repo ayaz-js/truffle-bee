e4f8d4349240687b104162e6d43b922a9988d9ef
Serik Ayazbayev <jetheez@yandex.ru>
2023-12-20 22:31:30 +0600

    remove: form text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,8 @@ export const Footer = ({}: FooterProps) => {
       <Line className="w-full" />
       {/*<FooterLogo className="w-full mx-auto mt-4" />*/}
       <p className="flex flex-col text-center mt-4">
-        &copy; {new Date().getFullYear()} Бутик Truffle Bee Алматы Шевченко 44А
+        &copy; {new Date().getFullYear()} Бутик Truffle Bee Алматы, улица
+        Шевченко 44А
         <a href="tel:+77070090029">+ 7 707 009 00 29</a>
       </p>
     </footer>
