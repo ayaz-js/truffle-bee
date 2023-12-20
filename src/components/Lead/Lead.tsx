8970cc62bf079d80ac05b6a02afdf4bf6f09fdf0
Serik Ayazbayev <jetheez@yandex.ru>
2023-12-20 22:27:41 +0600

    feat: style form, lead

--- src/components/Lead/Lead.tsx
+++ src/components/Lead/Lead.tsx
@@ -1,10 +1,8 @@
-interface LeadProps {}
-
-export const Lead = ({}: LeadProps) => {
+export const Lead = () => {
   return (
-    <section className="pt-[100px] sm:pt-32">
+    <section className="pt-24 sm:pt-32">
       <div className="max-w-screen-lg w-full">
-        <p className="text-2xl font-normal sm:text-5xl">
+        <p className="text-2xl font-normal sm:text-3xl">
           Для доступа к эксклюзивным распродажам, участия в специальных
           мероприятиях и получения персонализированных предложений, просим Вас
           заполнить следующие поля анкеты.
